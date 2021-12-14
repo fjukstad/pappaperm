cda3b7865f3039a97f75bf8cae700e867c991bf5
Bjørn Fjukstad <bjornfjukstad@gmail.com>
2021-12-14 20:15:45 +0100

    correct expires at

--- src/routes/api/callback.ts
+++ src/routes/api/callback.ts
@@ -33,8 +33,7 @@ export async function get({ query }) {
 		};
 	}
 
-	// Cookie expires 5 hours from now
-	const expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 5);
+	const expiresAt = new Date(response.expires_at * 1000);
 
 	return {
 		headers: {
