3e2eab84c643379350d9d00a1610f65a565a5ba5
Bjørn Fjukstad <bjornfjukstad@gmail.com>
2021-12-16 21:26:23 +0100

    add navbar with username

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,16 +15,20 @@
 
 		let totalTime = 0;
 		let totalDistance = 0;
+		let totalElevation = 0;
 		for (const activity of activities) {
 			const time = activity.elapsed_time;
 			const distance = activity.distance;
+			const elevation = activity.total_elevation_gain;
 			totalTime += time;
 			totalDistance += distance;
+			totalElevation += elevation;
 		}
 
 		let stats = {
 			totalTime: totalTime,
-			totalDistance: totalDistance
+			totalDistance: totalDistance,
+			totalElevation: totalElevation
 		};
 
 		const athlete = session.athlete;
@@ -70,13 +74,35 @@
 	});
 </script>
 
-<div class="">
-	<p>
-		Hei {athlete.firstname}!
-	</p>
+<div class="flex w-full text-right p-2 dark:bg-gray-700">
+	<div class="w-1">
+		<a href="/">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"
+				/>
+			</svg>
+		</a>
+	</div>
+	<div class="grow">
+		<p>
+			Hei {athlete.firstname}!
+		</p>
+	</div>
 </div>
-<div class="flex w-full justify-center mb-5 mt-2">
-	<div class="w-1/2">
+
+<div class="flex w-full justify-center mb-5">
+	<div class="w-1/5" />
+	<div class="grow pt-5">
 		<table class="table-auto w-full">
 			<tbody class="divide-y divide-gray-300">
 				<tr>
@@ -96,9 +122,14 @@
 					<td> Distanse</td>
 					<td class="text-right"> {Math.ceil(stats.totalDistance / 1000)} km</td>
 				</tr>
+				<tr>
+					<td> Høydemeter </td>
+					<td class="text-right"> {stats.totalElevation} m</td>
+				</tr>
 			</tbody>
 		</table>
 	</div>
+	<div class="w-1/5" />
 </div>
 
 <div class="h-full w-full">
