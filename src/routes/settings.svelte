5a611705c1fcab7e4f04f2de21f3e656a5a61781
Bjørn Fjukstad <bjornfjukstad@gmail.com>
2022-01-12 20:00:58 +0100

    print out all settings

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -12,8 +12,65 @@
 	export let settings;
 </script>
 
-Map latitude: {settings.lat} <br />
-Map Longitude: {settings.lng} <br />
-Acivity Type: {settings.activityType} <br />
-Start date: {settings.startDate} <br />
-End date: {settings.endDate} <br />
+<div class="flex w-full justify-center mb-5">
+	<div class="w-1/8" />
+	<div class="w-1/2 pt-10">
+		<form class="shadow-md dark:bg-gray-700 bg-gray-100 rounded px-8 pb-8 pt-8 mb-4">
+			<div class="mb-4">
+				<label class="block text-sm font-bold mb-2" for="lat"> Latitude </label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+					id="lat"
+					type="text"
+					value={settings.lat}
+					disabled
+				/>
+			</div>
+			<div class="mb-4">
+				<label class="block text-sm font-bold mb-2" for="lng"> Longitude </label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+					id="lng"
+					type="text"
+					value={settings.lng}
+					disabled
+				/>
+			</div>
+			<div class="mb-4">
+				<label class="block text-sm font-bold mb-2" for="activityType"> Activity Type</label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+					id="activityType"
+					type="text"
+					value={settings.activityType}
+					disabled
+				/>
+			</div>
+			<div class="mb-4">
+				<label class="block text-sm font-bold mb-2" for="startDate">Start Date</label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+					id="startDate"
+					type="text"
+					value={settings.startDate}
+					disabled
+				/>
+			</div>
+			<div class="mb-4">
+				<label class="block text-sm font-bold mb-2" for="startDate">End Date</label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+					id="endDate"
+					type="text"
+					value={settings.endDate}
+					disabled
+				/>
+			</div>
+			<div class="flex items-center justify-between">
+				<button class="bg-blue-500 text-white font-bold py-2 px-4 rounded " type="button" disabled>
+					Save
+				</button>
+			</div>
+		</form>
+	</div>
+</div>
