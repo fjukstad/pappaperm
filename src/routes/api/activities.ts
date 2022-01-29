ee8062988e13377cd62ac53c66cdfd5c6693f0f1
Bjørn Fjukstad <bjornfjukstad@gmail.com>
2022-01-29 19:33:02 +0100

    fetch more activities

--- src/routes/api/activities.ts
+++ src/routes/api/activities.ts
@@ -1,6 +1,6 @@
 export async function get({ params, locals }) {
 	let startDate = new Date(locals.settings.startDate).getTime() / 1000;
-	const url = 'https://www.strava.com/api/v3/athlete/activities?per_page=100&after=' + startDate;
+	const url = 'https://www.strava.com/api/v3/athlete/activities?per_page=200&after=' + startDate;
 	let activities = await fetch(url, {
 		method: 'GET',
 		headers: {
